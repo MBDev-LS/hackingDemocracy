00512bd4d09c68379479d9d455cb2a91d32b1912
Louis <louis@louisstevens.com>
2023-12-08 01:18:21 +0000

    Adds all webscraping functionality, except background story extraction.

--- websites_webscraping.py
+++ websites_webscraping.py
@@ -6,19 +6,26 @@ from pprint import pprint
 import re
 
 
-
 BACKGROUND_PAGE_TITLES = ['campaign', 'news', 'press releases', 'newsletter'] # 'newsletter' is redundant because of 'news'
+BASE_URL_REGEX = r'^(https?:\/\/([a-zA-Z0-9]|\.)+)'
+
+
+def get_base_url(url: str) -> str:
+	results = re.findall(BASE_URL_REGEX, url)
+	print(results)
+
+	return results[0][0] if len(results) > 0 else None
+
 
 def search_link_list(linkList: list, searchWord: str) -> list:
 	resultList = []
-	# print(linkList)
 
 	for linkElement in linkList:
-		# print(searchWord, linkElement.text.lower())
 		if searchWord in linkElement.text.lower():
 			resultList.append(linkElement)
 	
 	return resultList
+
 
 def get_unique_hrefs(linkList: list) -> set:
 	hrefSet = set()
@@ -32,17 +39,9 @@ def get_unique_hrefs(linkList: list) -> set:
 def extract_background_stories_from_page(url: str) -> list:
 	pass
 
-def get_base_url(url: str) -> str:
-	results = re.findall(r'^(https?:\/\/([a-zA-Z0-9]|\.)+)', url)
-	print(results)
-
-	return results[0] if len(results) > 0 else None
-
-print(get_base_url('https://www.jeremyhunt.org/'))
-
 
 def get_background_stories_from_website(url: str) -> list:
-	baseURL = 1
+	baseUrl = get_base_url(url)
 
 	try:
 		html = requests.get(url)
@@ -60,10 +59,12 @@ def get_background_stories_from_website(url: str) -> list:
 		uniqueHrefList = uniqueHrefList.union( get_unique_hrefs(relevantLinks) )
 
 	backgroundStoriesList = []
-
-	# for href in 1:
-	print(uniqueHrefList)
-
+	validUrlList = [baseUrl + hrefStr.replace(baseUrl, '') for hrefStr in uniqueHrefList]
+	
+	for validUrl in validUrlList:
+		backgroundStoriesList += extract_background_stories_from_page(validUrl)
+	
+	return backgroundStoriesList
 
 
 get_background_stories_from_website('https://www.jeremyhunt.org/')
